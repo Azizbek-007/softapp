8f19a66fb365ec5426b9fd699acd670d67909cc9
Azizbek-h <texnoposnode@gmail.com>
2023-01-19 17:41:24 +0500

    commits

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -10,6 +11,14 @@ async function bootstrap() {
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true}));
   
+  const config = new DocumentBuilder()
+    .setTitle('SoftApp')
+    .setDescription('The SoftApp API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   // render
   await app.listen(5000, () => console.log("🚀Listen on port 5000 🚀"));
 }
