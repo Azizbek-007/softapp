39bc9c7422d003d049cd34d9abc1fb91c3f8f90e
Azizbek-h <texnoposnode@gmail.com>
2023-01-20 23:47:40 +0500

    commit

---
+++ bots/5695738257.php
@@ -0,0 +1,72 @@
+<?php 
+$token = "5695738257:AAHM3de1FeBXsY8zvVb3McWqX7m2jRsAOLI";
+
+define('bot_token', $token);
+
+    function Answer(array $content)
+    {
+        return endpoint('sendMessage', $content);
+    }
+
+    function buildKeyBoard(array $options, $onetime = false, $resize = true, $selective = true)
+    {
+        $replyMarkup = [
+            'keyboard'          => $options,
+            'one_time_keyboard' => $onetime,
+            'resize_keyboard'   => $resize,
+            'selective'         => $selective,
+        ];
+        $encodedMarkup = json_encode($replyMarkup, true);
+        return $encodedMarkup;
+    }
+
+    function endpoint($api, array $content, $post = true)
+    {
+        $url = 'https://api.telegram.org/bot'.bot_token.'/'.$api;
+        if ($post) {
+            $reply = sendAPIRequest($url, $content);
+        } else {
+            $reply = sendAPIRequest($url, [], false);
+        }
+        return json_decode($reply, true);
+    }
+
+    function sendAPIRequest($url, array $content, $post = true)
+    {
+        if (isset($content['chat_id'])) {
+            $url = $url.'?chat_id='.$content['chat_id'];
+            unset($content['chat_id']);
+        }
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        if ($post) {
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
+        }
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        if ($result === false) {
+            $result = json_encode(['ok'=>false, 'curl_error_code' => curl_errno($ch), 'curl_error' => curl_error($ch)]);
+        }
+        curl_close($ch);
+        return $result;
+    }
+
+
+
+$efede = json_decode(file_get_contents('php://input'), true);
+
+$text = $efede["message"]["text"];
+$chat_id = $efede["message"]["chat"]["id"];
+
+
+
+if ($text == '/start') {
+
+    $content = ['chat_id' => $chat_id, 'text' => "Assalawma aleykum $ufname $uname.", 'parse_mode' => 'markdown'];
+    Answer($content);
+}
+
+     
