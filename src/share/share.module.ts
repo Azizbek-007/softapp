0ca8175994284ac5bcaf0d75848950a1d9594853
Azizbek-h <texnoposnode@gmail.com>
2023-01-22 19:05:49 +0500

    instrument share url created

--- src/share/share.module.ts
+++ src/share/share.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ShareService } from './share.service';
 import { ShareController } from './share.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Instrument } from 'src/instrument/entities/instrument.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ Instrument ])],
   controllers: [ShareController],
   providers: [ShareService]
 })
