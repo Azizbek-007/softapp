9670dd596b43bb903bee08980f45b78de32f65ac
Azizbek-h <texnoposnode@gmail.com>
2023-01-22 14:39:43 +0500

    support update dto

--- src/support/dto/update-support.dto.ts
+++ src/support/dto/update-support.dto.ts
@@ -2,7 +2,7 @@ import { PartialType } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 import { CreateSupportDto } from './create-support.dto';
 
-export class UpdateSupportDto extends PartialType(CreateSupportDto) {
+export class UpdateSupportDto {
     @IsString()
     answer: string;
 }
