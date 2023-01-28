ddc8345827813561f5fdc2b9db872e1f9aa3ddb1
Azizbek-h <texnoposnode@gmail.com>
2023-01-28 23:50:05 +0500

    fixed

--- src/order/order_lead.service.ts
+++ src/order/order_lead.service.ts
@@ -1,4 +1,4 @@
-import { Body, ConflictException, Injectable, NotFoundException } from "@nestjs/common";
+import { Body, ConflictException, Injectable, InternalServerErrorException, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Instrument } from "src/instrument/entities/instrument.entity";
 import { CreateLeadDto } from "src/leads/dto/create-lead.dto";
@@ -22,27 +22,29 @@ export class OrderLeadPublic extends LeadsService{
     async public_create (dto: CreateLeadDto) {
         dto.course = dto.courseId;
         dto.status = LeadsStatus.inactive;
-        let data = await this.create(dto);
-        console.log(data);   
+
+        let data = await this.create(dto);  
         dto.lead_course_id = `${data.id}${dto.courseId}`
+
         let new_order = this.OrderRepository.create({
-        FIO: data.FIO,
-        phone: data.phone,
-        lead: data,
-        course: dto.course,
-        lead_course_id:  dto.lead_course_id
+            FIO: data.FIO,
+            phone: data.phone,
+            lead: data,
+            course: dto.course,
+            lead_course_id:  dto.lead_course_id
         });
 
         try {
-        await new_order.save();
-        return new_order;
+            await new_order.save();
+            return new_order;
         } catch (error) {
-        if (error['code'] == 'ER_NO_REFERENCED_ROW_2') {
-            throw new NotFoundException("Not found course or lead id " + dto.course);
-        }
-        if (error['code'] == 'ER_DUP_ENTRY'){
-            throw new ConflictException();
-        }
+            if (error['code'] == 'ER_NO_REFERENCED_ROW_2') {
+                throw new NotFoundException("Not found course or lead id " + dto.course);
+            }
+            if (error['code'] == 'ER_DUP_ENTRY'){
+                throw new ConflictException();
+            }
+            throw new InternalServerErrorException(error.message)
         } 
     }
 }
