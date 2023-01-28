17b4d75dacf2b9bb8bacf56e755a6d3e06e119de
Azizbek-h <texnoposnode@gmail.com>
2023-01-28 23:51:05 +0500

    fixed

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -6,7 +6,6 @@ import { UpdateOrderDto } from './dto/update-order.dto';
 import { OrderLeadPublic } from './order_lead.service';
 import { CreateLeadDto } from 'src/leads/dto/create-lead.dto';
 
-@UseGuards(AuthGuard('jwt'))
 @Controller('order')
 export class OrderController {
   constructor(
@@ -19,26 +18,31 @@ export class OrderController {
     return this.orderLeadPublic.public_create(dto);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Post()
   create(@Body() createOrderDto: CreateOrderDto) {
     return this.orderService.create(createOrderDto);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   findAll(@Query() query) {
     return this.orderService.findAll(query);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.orderService.findOne(+id);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
     return this.orderService.update(+id, updateOrderDto);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.orderService.remove(+id);
